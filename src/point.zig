7aa3f6bf05ce2e2697e074ad57276f669744d918
Robert Sheehy <gameboy1092@gmail.com>
2025-10-14 09:49:32 -0500

    Setup initial simulation

--- src/point.zig
+++ src/point.zig
@@ -1,33 +1,50 @@
 const std = @import("std");
 
-pub const Point = struct {
-    x: i32,
-    y: i32,
+pub fn Point(comptime T: type) type {
+    return struct {
+        x: T,
+        y: T,
 
-    pub fn hash(self: @This()) u64 {
-        var h = std.hash.Wyhash.init(0);
-        h.update(std.mem.asBytes(&self.x));
-        h.update(std.mem.asBytes(&self.y));
-        return h.final();
-    }
+        const Self = @This();
 
-    pub fn eql(a: @This(), b: @This()) bool {
-        return a.x == b.x and a.y == b.y;
-    }
+        pub fn hash(self: Self) u64 {
+            var h = std.hash.Wyhash.init(0);
+            h.update(std.mem.asBytes(&self.x));
+            h.update(std.mem.asBytes(&self.y));
+            return h.final();
+        }
 
-    pub fn format(this: @This(), writer: *std.Io.Writer) !void {
-        try writer.print("Point({d}, {d})", .{ this.x, this.y });
-    }
-};
+        pub fn eql(a: Self, b: Self) bool {
+            return a.x == b.x and a.y == b.y;
+        }
+
+        pub fn format(this: Self, writer: *std.Io.Writer) !void {
+            try writer.print("Point({any}, {any})", .{ this.x, this.y });
+        }
+
+        pub fn neighbours(this: Self) [8]Self {
+            return [8]Self{
+                .{ .x = this.x - 1, .y = this.y - 1 },
+                .{ .x = this.x, .y = this.y - 1 },
+                .{ .x = this.x + 1, .y = this.y - 1 },
+                .{ .x = this.x - 1, .y = this.y },
+                .{ .x = this.x + 1, .y = this.y },
+                .{ .x = this.x - 1, .y = this.y + 1 },
+                .{ .x = this.x, .y = this.y + 1 },
+                .{ .x = this.x + 1, .y = this.y + 1 },
+            };
+        }
+    };
+}
 
 test "Point: eql, hash, and format" {
-    var p1 = Point{ .x = 3, .y = 4 };
-    var p2 = Point{ .x = 3, .y = 4 };
-    const p3 = Point{ .x = 4, .y = 3 };
+    var p1 = Point(i32){ .x = 3, .y = 4 };
+    var p2 = Point(i32){ .x = 3, .y = 4 };
+    const p3 = Point(i32){ .x = 4, .y = 3 };
 
     // eql
-    try std.testing.expect(Point.eql(p1, p2));
-    try std.testing.expect(!Point.eql(p1, p3));
+    try std.testing.expect(p1.eql(p2));
+    try std.testing.expect(!p1.eql(p3));
 
     // hash (deterministic & equal for equal points)
     const h1 = p1.hash();
@@ -39,18 +56,48 @@ test "Point: eql, hash, and format" {
 }
 
 test "Point: HashMap compatibility" {
-    var map = std.AutoHashMap(Point, u32).init(std.testing.allocator);
+    var map = std.AutoHashMap(Point(i32), u32).init(std.testing.allocator);
     defer map.deinit();
 
-    try map.put(Point{ .x = 1, .y = 2 }, 1);
-    try map.put(Point{ .x = 2, .y = 3 }, 2);
-    try map.put(Point{ .x = -2, .y = 4 }, 3);
-    try map.put(Point{ .x = 42, .y = 42 }, 1);
-    try map.put(Point{ .x = 42, .y = 42 }, 42);
+    try map.put(Point(i32){ .x = 1, .y = 2 }, 1);
+    try map.put(Point(i32){ .x = 2, .y = 3 }, 2);
+    try map.put(Point(i32){ .x = -2, .y = 4 }, 3);
+    try map.put(Point(i32){ .x = 42, .y = 42 }, 1);
+    try map.put(Point(i32){ .x = 42, .y = 42 }, 42);
 
-    try std.testing.expectEqual(map.get(Point{ .x = 1, .y = 2 }), 1);
-    try std.testing.expectEqual(map.get(Point{ .x = -2, .y = 4 }), 3);
-    try std.testing.expectEqual(map.get(Point{ .x = 2, .y = 3 }), 2);
-    try std.testing.expectEqual(map.get(Point{ .x = 2, .y = 1 }), null);
-    try std.testing.expectEqual(map.get(Point{ .x = 42, .y = 42 }), 42);
+    try std.testing.expectEqual(map.get(Point(i32){ .x = 1, .y = 2 }), 1);
+    try std.testing.expectEqual(map.get(Point(i32){ .x = -2, .y = 4 }), 3);
+    try std.testing.expectEqual(map.get(Point(i32){ .x = 2, .y = 3 }), 2);
+    try std.testing.expectEqual(map.get(Point(i32){ .x = 2, .y = 1 }), null);
+    try std.testing.expectEqual(map.get(Point(i32){ .x = 42, .y = 42 }), 42);
+}
+
+test "Point: Test neighbors" {
+    try std.testing.expectEqual(
+        (Point(i32){ .x = 0, .y = 0 }).neighbours(),
+        [8]Point(i32){
+            Point(i32){ .x = -1, .y = -1 },
+            Point(i32){ .x = 0, .y = -1 },
+            Point(i32){ .x = 1, .y = -1 },
+            Point(i32){ .x = -1, .y = 0 },
+            Point(i32){ .x = 1, .y = 0 },
+            Point(i32){ .x = -1, .y = 1 },
+            Point(i32){ .x = 0, .y = 1 },
+            Point(i32){ .x = 1, .y = 1 },
+        },
+    );
+
+    try std.testing.expectEqual(
+        (Point(i32){ .x = -100, .y = 100 }).neighbours(),
+        [8]Point(i32){
+            Point(i32){ .x = -101, .y = 99 },
+            Point(i32){ .x = -100, .y = 99 },
+            Point(i32){ .x = -99, .y = 99 },
+            Point(i32){ .x = -101, .y = 100 },
+            Point(i32){ .x = -99, .y = 100 },
+            Point(i32){ .x = -101, .y = 101 },
+            Point(i32){ .x = -100, .y = 101 },
+            Point(i32){ .x = -99, .y = 101 },
+        },
+    );
 }
--- src/root.zig
+++ src/root.zig
@@ -1,18 +1,7 @@
 const std = @import("std");
+pub const Cell = @import("cell.zig").Cell;
 pub const Point = @import("point.zig").Point;
-
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try stdout.flush(); // Don't forget to flush!
-}
+pub const Simulation = @import("simulation.zig").Simulation;
 
 test "visit all decls so their tests are found" {
     std.testing.refAllDecls(@This());
